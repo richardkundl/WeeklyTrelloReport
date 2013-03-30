ef39457da879a72da360186b2c50ca8ab0ebb1f7
Richard Kundl <kundl.richard@gmail.com>
2013-03-30 21:50:58 +0100

    recreated ajax handler for jquery 1.9.1

--- TrelloReport/TrelloReport/Scripts/application.js
+++ TrelloReport/TrelloReport/Scripts/application.js
@@ -1,43 +1,92 @@
-﻿/// <reference path="jquery-1.4.1-vsdoc.js" />
+﻿/// <reference path="jquery-1.9.1-vsdoc.js" />
 
-function IsAuthenticated(url) {
-    $.ajax({
+var isConsoleLogging = true;
+function logError(error) {
+    if (isConsoleLogging == true) {
+        console.log(error);
+    }
+    else {
+        alert(error);
+    }
+}
+
+var urlIsAuthenticated = "/api/isauthenticated";
+var urlFillBoards = "/api/getboards";
+var urlFillLists = "/api/getlists";
+
+
+function IsAuthenticatedSucces(result) {
+    if (result.isLogged == true) {
+        // logged in
+        $("a#trello-authorize").hide();
+        FillBoards();
+    } else {
+        // not logged in
+        $("a#trello-authorize").attr("href", result.authUrl);
+    }
+};
+
+function FillBoardsSucces(result) {
+    $.each(result, function () {
+        var options = $("select#trello-boards");
+        options.append($("<option />").val(this.Id).text(this.Name));
+    });
+};
+
+function FillListsSucces(result) {
+    alert(result);
+}
+
+function IsAuthenticated() {
+    var data = null;
+    var request = $.ajax({
+        url: urlIsAuthenticated,
         type: "POST",
-        url: url,
-        data: "",
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result) {
-            if (result.isLogged == true) {
-                // logged in
-                $("a#trello-authorize").hide();
-                FillBoards("/Api/GetBoards");
-            } else {
-                // not logged in
-                $("a#trello-authorize").attr("href", result.authUrl);
-            }
-        },
-        error: function (e) {
-            alert(e);
-        }
+        data: data,
+        dataType: "json"
+    });
+
+    request.done(function (result) {
+        IsAuthenticatedSucces(result);
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        logError(textStatus);
     });
 }
 
-function FillBoards(url) {
-    $.ajax({
+function FillBoards() {
+    var data = null;
+    var request = $.ajax({
+        url: urlFillBoards,
         type: "POST",
-        url: url,
-        data: "",
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result) {
-            $.each(result, function () {
-                var options = $("select#trello-boadrs");
-                options.append($("<option />").val(this.Id).text(this.Name));
-            });
-        },
-        error: function (e) {
-            alert(e);
-        }
+        data: data,
+        dataType: "json"
+    });
+
+    request.done(function (result) {
+        FillBoardsSucces(result);
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        logError(textStatus);
+    });
+}
+
+function FillLists(boardId) {
+    var data = { boardId: boardId };
+    var request = $.ajax({
+        url: urlFillLists,
+        type: "POST",
+        data: data,
+        dataType: "json"
+    });
+
+    request.done(function (result) {
+        FillListsSucces(result);
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        logError(textStatus);
     });
 }
