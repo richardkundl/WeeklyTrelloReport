dc9a17d8880e7f8d7882f23c5c5cdc6265d3d3b6
Richard Kundl <kundl.richard@gmail.com>
2013-04-02 22:11:37 +0200

    date module functions

--- TrelloReport/TrelloReport/Scripts/application.js
+++ TrelloReport/TrelloReport/Scripts/application.js
@@ -1,4 +1,39 @@
 ﻿/// <reference path="jquery-1.9.1-vsdoc.js" />
+Date.prototype.getWeek = function () {
+    var onejan = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+};
+
+function toTwoDigitNumber(number) {
+    if(number.toString().length > 1) {
+        return number;
+    }
+    
+    return '0' + number; 
+}
+
+function startDateOfWeek(weekNo) {
+    var d1 = new Date();
+    var numOfdaysPastSinceLastMonday = eval(d1.getDay() - 1);
+    d1.setDate(d1.getDate() - numOfdaysPastSinceLastMonday);
+    var weekNoToday = d1.getWeek();
+    var weeksInTheFuture = eval(weekNo - weekNoToday);
+    d1.setDate(d1.getDate() + eval(7 * weeksInTheFuture));
+    var rangeIsFrom = d1.getFullYear() + "-" + toTwoDigitNumber(eval(d1.getMonth() + 1)) + "-" + toTwoDigitNumber(d1.getDate());
+    return rangeIsFrom;
+};
+
+function endDateOfWeek(weekNo) {
+    var d1 = new Date();
+    var numOfdaysPastSinceLastMonday = eval(d1.getDay() - 1);
+    d1.setDate(d1.getDate() - numOfdaysPastSinceLastMonday);
+    var weekNoToday = d1.getWeek();
+    var weeksInTheFuture = eval(weekNo - weekNoToday);
+    d1.setDate(d1.getDate() + eval(7 * weeksInTheFuture));
+    d1.setDate(d1.getDate() + 6);
+    var rangeIsTo = d1.getFullYear() + "-" + toTwoDigitNumber(eval(d1.getMonth() + 1)) + "-" + toTwoDigitNumber(d1.getDate());
+    return rangeIsTo;
+};
 
 var isConsoleLogging = true;
 function logError(error) {
