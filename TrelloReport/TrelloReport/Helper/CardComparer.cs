7c9b8bcec99679483b1e70e8092c927ff9f69792
Richard Kundl <kundl.richard@gmail.com>
2013-04-08 16:04:37 +0200

    Add IEqualityComparer<List<Card.Label>>

--- TrelloReport/TrelloReport/Helper/CardComparer.cs
+++ TrelloReport/TrelloReport/Helper/CardComparer.cs
@@ -4,7 +4,7 @@ using TrelloNet;
 /// <summary>
 /// Trello card comparer
 /// </summary>
-public class CardComparer: IComparer<List<Card.Label>>
+public class CardComparer : IComparer<List<Card.Label>>, IEqualityComparer<List<Card.Label>>
 {
     /// <summary>
     /// Custom compare to card labels
@@ -52,5 +52,38 @@ public class CardComparer: IComparer<List<Card.Label>>
         }
 
         return 0;
+    }
+
+    /// <summary>
+    /// Override base.Equals
+    /// </summary>
+    /// <param name="x">Card labels</param>
+    /// <param name="y">Card labels</param>
+    /// <returns>is equals</returns>
+    public bool Equals(List<Card.Label> x, List<Card.Label> y)
+    {
+        if (Compare(x, y) == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Generate custom hashcode from labels
+    /// </summary>
+    /// <param name="obj">card labels</param>
+    /// <returns>hash code</returns>
+    public int GetHashCode(List<Card.Label> obj)
+    {
+        int hash = 13;
+        foreach (var label in obj)
+        {
+            hash = (hash * 7) + label.Name.GetHashCode();
+            hash = (hash * 7) + label.Color.GetHashCode();
+        }
+
+        return hash;
     }
 }
