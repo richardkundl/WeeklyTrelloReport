d73774243acb80b1592a267c4cce56704ca689d4
Richard Kundl <kundl.richard@gmail.com>
2013-04-09 13:45:35 +0200

    file name append actual date

--- TrelloReport/TrelloReport/Controllers/ExportController.cs
+++ TrelloReport/TrelloReport/Controllers/ExportController.cs
@@ -4,6 +4,7 @@ using TrelloNet;
 using TrelloReport.Models;
 using TrelloReport.Helper;
 using TrelloReport.Service;
+using System;
 
 namespace TrelloReport.Controllers
 {
@@ -14,9 +15,10 @@ namespace TrelloReport.Controllers
             var cards = CardHelper.GetCards(model, TrelloInstance).ToList();
             var lists = TrelloInstance.Lists.ForBoard(new BoardId(model.BoardId)).OrderBy(l => l.Pos).ToList();
             var result = new Sharp2WordService().GenerateCardReports(cards, lists);
+            var fileName = string.Format("trello-report-{0}.doc", DateTime.Now.ToString("yyyy-MM-dd"));
             return File(result,
                 "application/vnd.ms-word",
-                "mytestfile.doc");
+                fileName);
         }
 
         public ActionResult Excel(ReportModel model)
